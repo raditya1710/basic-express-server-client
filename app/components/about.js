1cd0b1d8380a9f1e9fc2c580f1fbaaf6432fe26a
Alfonsus Raditya Arsadjaja <raditya1710@gmail.com>
2016-08-04 15:04:54 +0700

    Added NotFound for all unintended pages and Photos Page

--- app/components/about.js
+++ app/components/about.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router';
 
 export default class About extends Component{
   render(){
-    const imgUrl = '../image/DSCN7542.JPG';
+    const imgUrl = '../image/Optimized-DSCN7542.JPG';
     var divStyle = {
       background: 'url(' + imgUrl + ')',
       backgroundSize:'100% 100%',
@@ -14,15 +14,19 @@ export default class About extends Component{
     return(
       <div style={divStyle}>
         <div className="text-xs-center">
+          <Link className="btn btn-primary btn-sm" to="/home">Home</Link>
+          <Link className="btn btn-primary btn-sm" to="/photos">Photos</Link>
+          <Link className="btn btn-primary btn-sm" to="/users">Users</Link>
+        </div>
+        <div className="text-xs-center">
             <div className="text-info">
                 Alfonsus Raditya Arsadjaja <br />
                 GDP Intern Summer 2016 <br/>
                 Copyright (c) 2016 <br />
             </div>
             <br/> <br/>
-            <Link className="btn btn-primary" to="/home">Home</Link>
-            <Link className="btn btn-primary" to="/users">Users</Link>
         </div>
+
       </div>
     );
   }
